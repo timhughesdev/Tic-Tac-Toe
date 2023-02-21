a38be211e0d9a6f1ebb98e71d6f5101d8d179490
th2n10s4u <th2n10s4u@gmail.com>
2023-02-21 11:46:18 -0800

    Update server.js
    Changed port from 3000 to process.envPORT || 3001

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3001;
 // const path = require("path");
 
 app.use(express.static("client"));
