37cad3f530f76da41ee5484972cb1882b5bf3558
th2n10s4u <th2n10s4u@gmail.com>
2023-02-21 14:01:56 -0800

    Update server.js
    tinkering

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3001;
 // app.use(express.static("client"));
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname, "client", "index.html");
+  res.sendFile(__dirname, "./index.html");
 }); // Express.js is not able to render the static css and JS file from public folder using sendFile. Use middleware express.static method
 
 app.listen(port, () => {
