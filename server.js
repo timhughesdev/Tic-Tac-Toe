9dcda2d7041f804a8ebaa5a1943c03e4b4a1b346
th2n10s4u <th2n10s4u@gmail.com>
2023-02-21 13:41:10 -0800

    Update server.js
    tinkering

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const port = process.env.PORT || 3001;
 // const path = require("path");
 
 // app.use(express.static("client"));
-app.get("/", (req, res) => res.type("html").send(html));
-// app.get("/", (req, res) => {
-//   res.sendFile(__dirname, "client", "index.html");
-// }); // Express.js is not able to render the static css and JS file from public folder using sendFile. Use middleware express.static method
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname, "client", "index.html");
+}); // Express.js is not able to render the static css and JS file from public folder using sendFile. Use middleware express.static method
 
 app.listen(port, () => {
   console.log(`Port is listening on: ${port}`);
