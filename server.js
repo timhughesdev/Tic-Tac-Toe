2d4874b513dc059ccc53cab6bf14b981077bded4
th2n10s4u <th2n10s4u@gmail.com>
2023-02-21 13:22:23 -0800

    Update server.js
    Using expressjs' hello world example for app.get

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const app = express();
 const port = process.env.PORT || 3001;
 // const path = require("path");
 
-app.use(express.static("client"));
-
+// app.use(express.static("client"));
+app.get("/", (req, res) => res.type("html").send(html));
 // app.get("/", (req, res) => {
 //   res.sendFile(__dirname, "client", "index.html");
 // }); // Express.js is not able to render the static css and JS file from public folder using sendFile. Use middleware express.static method
