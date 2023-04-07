954ec3e91484e73063101bb8fb66d4f5f9a5b7db
th2n10s4u <th2n10s4u@gmail.com>
2023-04-06 23:57:00 -0700

    Update server.js
    Express server ping button

--- server.js
+++ server.js
@@ -1,14 +1,34 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3001;
-// const path = require("path");
+import Express from "express";
+import http from "http";
+import startSocketServer from "./socket";
 
-app.use(express.static("client"));
+const app = Express();
+const server = http.createServer(app);
+const port = 3001;
 
-// app.get("/", (req, res) => {
-//   res.sendFile(__dirname, "client");
-// }); // Express.js is not able to render the static css and JS file from public folder using sendFile. Use middleware express.static method
+app.use("/", Express.static("client"));
 
-app.listen(port, () => {
-  console.log(`Port is listening on: ${port}`);
+//check to see if the server is sending/receiving
+app.get("/ping", (req, res) => {
+  res.status(200).json({ ping: "Pong!" });
 });
+
+server.listen(port, () => {
+  console.log(`Server is listening on: ${port}`);
+});
+
+// -----Previous Implementation of Express Server-----//
+// const express = require("express");
+// const app = express();
+// const port = process.env.PORT || 3001;
+// // const path = require("path");
+
+// app.use(express.static("client"));
+
+// // app.get("/", (req, res) => {
+// //   res.sendFile(__dirname, "client");
+// // }); // Express.js is not able to render the static css and JS file from public folder using sendFile. Use middleware express.static method
+
+// app.listen(port, () => {
+//   console.log(`Port is listening on: ${port}`);
+// });
